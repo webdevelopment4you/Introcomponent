b79e757a1dd331791c6eb8373b81dcb4632dd780
webdevelopment4you <65403221+webdevelopment4you@users.noreply.github.com>
2021-01-23 10:48:25 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let image = document.getElementById("backgroundImage");
+
+let changeBackground = () => {
+    if (document.body.offsetWidth < 768) {
+        image.src = "images/bg-intro-mobile.png";
+        console.log(image.src);
+    }
+    else {
+        image.src = "images/bg-intro-desktop.png";
+    }
+}
+
